a2147bd84c56c48fa2755d144b0e1f752f7334a8
Scott Fletcher <scott@amazing-outcomes.de>
2023-07-26 15:00:17 +0200

    add game object, scoreboard

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ class Player {
     scorePoint() {}
 }
 
+class Game {
+    constructor(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    scoreboard = 'love - love';
+    getScoreboard() {
+        return this.scoreboard;
+    }
+}
+
 const player1 = new Player(0);
+const player2 = new Player(0);
+
+const game = new Game(player1, player2);
 
 module.exports = {
-    player: new Player(0)
+    player: new Player(0),
+    game
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,19 @@
-const {player} = require('index.js');
+const {player, game} = require('./index.js');
 
-test('player starts with a score of 0', () => {
-    expect(player.score).toBe(0);
-  });
+describe('Tennis Game', () => {
+
+    test('player starts with a score of 0', () => {
+        expect(player.score).toBe(0);
+    });
+
+    test('game starts with two players, who both have zero points', () => {
+        expect(game.player1).toBeDefined();
+        expect(game.player2).toBeDefined();
+        expect(game.player1.score).toBe(0);
+        expect(game.player2.score).toBe(0);
+    });
+
+    test('game starts with score "love - love"', () => {
+        expect(game.scoreboard).toBe('love - love');
+    });
+})
