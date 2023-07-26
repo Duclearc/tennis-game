2d5b779e3a775de2d41d5394469bf39b1ea4f321
Dan Pina <dan@duclearc.fritz.box>
2023-07-26 15:33:55 +0200

    started advantage logic

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 class Player {
-    constructor(score) {
-        this.score = score
-    }
+  constructor(score) {
+    this.score = score;
+  }
 
-    scorePoint() {
-        this.score++;
-    }
+  scorePoint() {
+    this.score++;
+  }
 }
 
 class Game {
-    constructor(player1, player2) {
-        this.player1 = player1;
-        this.player2 = player2;
-    }
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+  }
 
-    scores = ["love", "15", "30", "40"];
-    scoreboard = 'love - love';
+  scores = ["love", "15", "30", "40"];
+  scoreboard = "love - love";
+  deuce = "deuce";
 
-    getScoreboard() {
-        return this.scoreboard;
-    }
+  getScoreboard() {
+    return this.scoreboard;
+  }
 
-    updateScoreboard() {
-        if (this.player1.score >= 4) {
-            this.scoreboard = "player one wins";
-        } else {
-            this.scoreboard = `${this.scores[player1.score]} - ${this.scores[player2.score]}`
-        }
+  updateScoreboard() {
+    if (
+      this.player1.score == 4 && this.player2.score == 4 
+    //   (&& this.player1.score == this.player2.score)
+    ) {
+        this.scoreboard = deuce;
+    } else if (this.scoreboard == this.deuce && this.player1.score == 5) {
+        this.scoreboard = "player one advantage";
     }
+    else if (this.player1.score >= 4) {
+      this.scoreboard = "player one wins";
+    } else if (this.player2.score >= 4) {
+      this.scoreboard = "player two wins";
+    } else {
+      this.scoreboard = `${this.scores[player1.score]} - ${
+        this.scores[player2.score]
+      }`;
+    }
+  }
 }
 
 const player1 = new Player(0);
@@ -36,8 +49,8 @@ const player2 = new Player(0);
 const game = new Game(player1, player2);
 
 module.exports = {
-    player: new Player(0),
-    game,
-    Player,
-    Game
-}
+  player: new Player(0),
+  game,
+  Player,
+  Game,
+};
--- index.test.js
+++ index.test.js
@@ -1,60 +1,77 @@
-const {player, game, Game, Player} = require("./index.js");
+const { player, game, Game, Player } = require("./index.js");
 
 describe("Tennis Game", () => {
-    test("player starts with a score of 0", () => {
-        expect(player.score).toBe(0);
-    });
+  test("player starts with a score of 0", () => {
+    expect(player.score).toBe(0);
+  });
 
-    test("game starts with two players, who both have zero points", () => {
-        expect(game.player1).toBeDefined();
-        expect(game.player2).toBeDefined();
-        expect(game.player1.score).toBe(0);
-        expect(game.player2.score).toBe(0);
-    });
+  test("game starts with two players, who both have zero points", () => {
+    expect(game.player1).toBeDefined();
+    expect(game.player2).toBeDefined();
+    expect(game.player1.score).toBe(0);
+    expect(game.player2.score).toBe(0);
+  });
 
-    test('game starts with score "love - love"', () => {
-        expect(game.scoreboard).toBe("love - love");
-    });
+  test('game starts with score "love - love"', () => {
+    expect(game.scoreboard).toBe("love - love");
+  });
 
-    test('player successfully scores a point', () => {
-        player.scorePoint();
-        expect(player.score).toBe(1);
-    });
+  test("player successfully scores a point", () => {
+    player.scorePoint();
+    expect(player.score).toBe(1);
+  });
 
-    test('player scores twice', () => {
-        player.scorePoint();
-        expect(player.score).toBe(2);
-    })
+  test("player scores twice", () => {
+    player.scorePoint();
+    expect(player.score).toBe(2);
+  });
 
-    test('player one scores, scoreboard shows "15 - love"', () => {
-        game.player1.scorePoint();
-        game.updateScoreboard();
-        expect(game.getScoreboard()).toBe("15 - love");
-    });
+  test('player one scores, scoreboard shows "15 - love"', () => {
+    game.player1.scorePoint();
+    game.updateScoreboard();
+    expect(game.getScoreboard()).toBe("15 - love");
+  });
 
-    test('player two scores, scoreboard shows "15 - 15"', () => {
-        game.player2.scorePoint();
-        game.updateScoreboard();
-        expect(game.getScoreboard()).toBe("15 - 15");
-    });
+  test('player two scores, scoreboard shows "15 - 15"', () => {
+    game.player2.scorePoint();
+    game.updateScoreboard();
+    expect(game.getScoreboard()).toBe("15 - 15");
+  });
 
-    test('player one wins (not in advantage), scoreboard shows "player one wins"', () => {
-        const game1 = new Game(new Player(0), new Player(0));
-        game1.player1.scorePoint();
-        game1.player1.scorePoint();
-        game1.player1.scorePoint();
-        game1.player1.scorePoint();
-        game1.updateScoreboard();
-        expect(game1.getScoreboard()).toBe("player one wins");
-    });
+  test('player one wins (not in advantage), scoreboard shows "player one wins"', () => {
+    const game1 = new Game(new Player(0), new Player(0));
+    game1.player1.scorePoint();
+    game1.player1.scorePoint();
+    game1.player1.scorePoint();
+    game1.player1.scorePoint();
+    game1.updateScoreboard();
+    expect(game1.getScoreboard()).toBe("player one wins");
+  });
 
-    test('player two wins (not in advantage), scoreboard shows "player two wins"', () => {
-        const game1 = new Game(new Player(0), new Player(0));
-        game1.player2.scorePoint();
-        game1.player2.scorePoint();
-        game1.player2.scorePoint();
-        game1.player2.scorePoint();
-        game1.updateScoreboard();
-        expect(game1.getScoreboard()).toBe("player two wins");
-    });
+  test('player two wins (not in advantage), scoreboard shows "player two wins"', () => {
+    const game1 = new Game(new Player(0), new Player(0));
+    game1.player2.scorePoint();
+    game1.player2.scorePoint();
+    game1.player2.scorePoint();
+    game1.player2.scorePoint();
+    game1.updateScoreboard();
+    expect(game1.getScoreboard()).toBe("player two wins");
+  });
+
+  test("game is deuce", () => {
+    const game1 = new Game(new Player(0), new Player(0));
+    // player 2 gets 40 pts
+    game1.player2.scorePoint();
+    game1.player2.scorePoint();
+    game1.player2.scorePoint();
+    game1.player2.scorePoint();
+    // player 1 gets 40 pts
+    game1.player1.scorePoint();
+    game1.player1.scorePoint();
+    game1.player1.scorePoint();
+    game1.player1.scorePoint();
+
+    game1.updateScoreboard();
+    expect(game1.getScoreboard()).toBe("deuce");
+  });
 });
